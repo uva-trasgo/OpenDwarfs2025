43a02df78ce55865cfc2134276f65e52e5fd9bcd
danlopeUVa <daniel.lopezm@uva.es>
2026-07-13 16:51:07 +0200

    Enabled FPGA kernel compilation of  nw and bfs for emulation platform

--- dynamic-programming/nw/Makefile.mk
+++ dynamic-programming/nw/Makefile.mk
@@ -15,7 +15,25 @@ exec_local += nw-exec-local
 nw-all-local:
 	cp $(top_srcdir)/dynamic-programming/nw/needle_kernel.cl .
 	cp $(top_srcdir)/dynamic-programming/nw/needle_kernel_opt_gpu.cl .
+	cp $(top_srcdir)/dynamic-programming/nw/needle_fpga.cl .
 
 nw-exec-local:
 	cp $(top_srcdir)/dynamic-programming/nw/needle_kernel.cl ${DESTDIR}${bindir}
 	cp $(top_srcdir)/dynamic-programming/nw/needle_kernel_opt_gpu.cl ${DESTDIR}${bindir}
+	cp $(top_srcdir)/dynamic-programming/nw/needle_fpga.cl ${DESTDIR}${bindir}
+
+# New: Compilation targets for FPGA Emulation if --enable-aot-emulation is passed
+if BUILD_AOT_EMULATION
+all_local += needle_kernel_opt_fpga.aocx needle_kernel.aocx
+exec_local += dwarf-needle-fpga-exec-local
+
+needle_kernel.aocx: $(top_srcdir)/dynamic-programming/nw/needle_kernel.cl
+	$(AOC_COMPILER) -march=emulator $< -o $@
+
+needle_kernel_opt_fpga.aocx: $(top_srcdir)/dynamic-programming/nw/needle_fpga.cl
+	$(AOC_COMPILER) -march=emulator $< -o $@
+
+dwarf-needle-fpga-exec-local:
+	cp needle_kernel.aocx ${DESTDIR}${bindir}
+	cp needle_kernel_opt_fpga.aocx ${DESTDIR}${bindir}
+endif
--- graph-traversal/bfs/Makefile.mk
+++ graph-traversal/bfs/Makefile.mk
@@ -20,6 +20,24 @@ exec_local += bfs-exec-local
 
 bfs-all-local:
 	cp $(top_srcdir)/graph-traversal/bfs/bfs_kernel.cl .
+	cp $(top_srcdir)/graph-traversal/bfs/bfs_fpga.cl .
 
 bfs-exec-local:
 	cp $(top_srcdir)/graph-traversal/bfs/bfs_kernel.cl ${DESTDIR}${bindir}
+	cp $(top_srcdir)/graph-traversal/bfs/bfs_fpga.cl ${DESTDIR}${bindir}
+
+# New: Compilation targets for FPGA Emulation if --enable-aot-emulation is passed
+if BUILD_AOT_EMULATION
+all_local += bfs_kernel_opt_fpga.aocx bfs_kernel.aocx
+exec_local += dwarf-bfs-fpga-exec-local
+
+bfs_kernel.aocx: $(top_srcdir)/graph-traversal/bfs/bfs_kernel.cl
+	$(AOC_COMPILER) -march=emulator $< -o $@
+
+bfs_kernel_opt_fpga.aocx: $(top_srcdir)/graph-traversal/bfs/bfs_fpga.cl
+	$(AOC_COMPILER) -march=emulator $< -o $@
+
+dwarf-bfs-fpga-exec-local:
+	cp bfs_kernel.aocx ${DESTDIR}${bindir}
+	cp bfs_kernel_opt_fpga.aocx ${DESTDIR}${bindir}
+endif
